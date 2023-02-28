a88c583b0752999f143cc5b244342b1fc580657b
Daniel-Mc-Carthy <22348506@studentmail.ul.ie>
2023-02-28 01:05:37 +0000

    Initial Push

---
+++ setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+
+    name='roppit',
+    version = '0.1',
+    author = "Daniel Mc Carthy",
+    author_email = "daniel.mccarthy@roppit.com",
+    description = "This is the first version",
+    packages = setuptools.find_packages(),
+
+)
